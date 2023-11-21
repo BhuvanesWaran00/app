fb588881efe065ef7c6c35130388466dec0c2887
bhuvaneswaran <bwaran001@gamil.com>
2023-11-22 01:06:27 +0530

    sql updated

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, redirect
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'admin'                                                                     # 'your_mysql_username'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin123'                                                              # 'your_mysql_password'
+app.config['MYSQL_DATABASE_DB'] = 'data'                                                                        # 'your_mysql_database'
+app.config['MYSQL_DATABASE_HOST'] = 'test.cwyp9bpkbyeg.ap-south-1.rds.amazonaws.com'                            # 'your_mysql_host'
+
+mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
@@ -11,8 +21,26 @@ def signup():
     name = request.form.get('name')
     email = request.form.get('mail')
     password = request.form.get('pass')
-    confirm_password = request.form.get('con_pass')
-    print(name,email)
+
+        # Create a MySQL cursor
+    cursor = mysql.get_db().cursor()
+
+    # Insert data into the 'users' table
+    cursor.execute("INSERT INTO users (Name, Email, Password) VALUES (%s, %s, %s)", (name, email, password))
+
+    # Commit changes to the database
+    mysql.get_db().commit()
+
+    # Close the cursor
+    cursor.close()
+
+    return render_template('signin.html')
+
+@app.route('/signin.html')
+def signin():
+    name = request.form.get('username')
+    password = request.form.get('password')
+    # Add any logic you need for the signin route
     return render_template('signin.html')
     
 if __name__ == '__main__':
